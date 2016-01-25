24c4cd437203899beda9acd236b9681a2c6c5e7d
talisk <talisk@foxmail.com>
2016-01-25 21:42:29 +0800

    Change course model with teacher and student data.

--- iHBNU/Model/Course.h
+++ iHBNU/Model/Course.h
@@ -13,13 +13,30 @@
 @property (nonatomic, copy) NSString *beizhu;
 @property (nonatomic, copy) NSString *beizhu1;
 
-@property (nonatomic, copy) NSString *userid;
-@property (nonatomic, copy) NSString *courseName;
-@property (nonatomic, copy) NSString *venue;
-@property (nonatomic, copy) NSString *courseSessionString;
-@property (nonatomic, copy) NSString *selectionid;
-@property (nonatomic, copy) NSString *year;
-@property (nonatomic, copy) NSString *semester;
+/**
+ *  Public variable.
+ */
+@property (nonatomic, copy) NSString *courseid;     // 课程标识
+@property (nonatomic, copy) NSString *courseName;   // 课程名
+@property (nonatomic, copy) NSString *year;         // 学年
+@property (nonatomic, copy) NSString *semester;     // 学期
+@property (nonatomic, copy) NSString *classroom;       // 教室名
+@property (nonatomic, copy) NSString *courseTimes;   // 上课时间
+
+/**
+ *  Student's variable.
+ */
+@property (nonatomic, copy) NSString *studentid;    // 学生ID
+
+/**
+ *  Teacher's variable.
+ */
+@property (nonatomic, copy) NSString *teacherid;    // 教师ID
+@property (nonatomic, copy) NSString *courseProperty;     // 课程属性
+@property (nonatomic, copy) NSString *academy;      // 开课学院
+@property (nonatomic, copy) NSString *courseCode;     // 课程属性
+@property (nonatomic, copy) NSString *studentCount; // 学生数
+@property (nonatomic, copy) NSString *classes; // 班级
 
 @property (nonatomic, copy) NSString *message;
 
--- iHBNU/Model/Course.m
+++ iHBNU/Model/Course.m
@@ -10,17 +10,52 @@
 
 @implementation Course
 
+///**
+// *  Public variable.
+// */
+//@property (nonatomic, copy) NSString *courseid;     // 课程标识
+//@property (nonatomic, copy) NSString *courseName;   // 课程名
+//@property (nonatomic, copy) NSString *year;         // 学年
+//@property (nonatomic, copy) NSString *semester;     // 学期
+//@property (nonatomic, copy) NSString *classroom;       // 教室名
+//@property (nonatomic, copy) NSString *courseTimes;   // 上课时间
+//
+///**
+// *  Student's variable.
+// */
+//@property (nonatomic, copy) NSString *studentid;    // 学生ID
+//
+///**
+// *  Teacher's variable.
+// */
+//@property (nonatomic, copy) NSString *teacherid;    // 教师ID
+//@property (nonatomic, copy) NSString *property;     // 课程属性
+//@property (nonatomic, copy) NSString *academy;      // 开课学院
+//@property (nonatomic, copy) NSString *courseCode;     // 课程属性
+//@property (nonatomic, strong) NSNumber *studentCount; // 学生数
+//@property (nonatomic, copy) NSString *classes; // 班级
+//
+//@property (nonatomic, copy) NSString *message;
+
 +(NSDictionary *)JSONKeyPathsByPropertyKey {
     return @{@"beizhu" : @"beizhu",
              @"beizhu1" : @"beizhu1",
              
-             @"userid" : @"id",
+             @"courseid" : @"xkkh",
              @"courseName" : @"kcm",
-             @"venue" : @"skdd",
-             @"courseSessionString" : @"sksj",
-             @"selectionid" : @"xkkh",
              @"year" : @"xn",
              @"semester" : @"xq",
+             @"classroom" : @"skdd",
+             @"courseTimes" : @"sksj",
+             
+             @"studentid" : @"id",
+             
+             @"teacherid" : @"teaid",
+             @"courseProperty" : @"kcxz",
+             @"academy" : @"kkxy",
+             @"courseCode" : @"kcdh",
+             @"studentCount" : @"numpp",
+             @"classes" : @"bj",
              
              @"message" : @"message"
              };
