385e6011272466543db9739fc1ee6d9152f6a1e8
talisk <talisk@foxmail.com>
2016-01-26 22:43:39 +0800

    Modify courseTime model.

--- iHBNU/Model/CourseTime.h
+++ iHBNU/Model/CourseTime.h
@@ -12,6 +12,8 @@
 
 @property (nonatomic, strong) NSNumber *weekday; // 星期几 1,2,3,4,5,6,7
 @property (nonatomic, strong) NSArray<NSNumber *> *sequence; // 第几节课 1,2,3,4,5,6,7,8,9,10
+@property (nonatomic, strong) NSNumber *startWeek; // 开始周 1,2,3,4,5,6,7...
+@property (nonatomic, strong) NSNumber *endWeek; // 结束周 1,2,3,4,5,6,7...
 
 @property BOOL oddWeek;
 @property BOOL evenWeek;
