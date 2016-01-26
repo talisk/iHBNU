278c67c4c768d9b74139db18db577389311e73d4
talisk <talisk@foxmail.com>
2016-01-27 00:12:04 +0800

    Override description method.

--- iHBNU/Model/CoursePackage.m
+++ iHBNU/Model/CoursePackage.m
@@ -10,4 +10,17 @@
 
 @implementation CoursePackage
 
+- (NSString *)description {
+    return [NSString stringWithFormat:@"\n\tcourseid:%@\n\tcourseName:%@\n\tyear:%@\n\tsemester:%@\n\tclassroom:%@\n\tcourseTimes:%@\n\tstudentid:%@\n\tteacherid:%@\n\tcourseProperty:%@\n\tacademy:%@\n\tcourseCode:%@\n\tstudentCount:%@\n\tclasses:%@",self.courseid,self.courseName,self.year,self.semester,self.classroom,self.courseTimes.description,self.studentid,self.teacherid,self.courseProperty,self.academy,self.courseCode,self.studentCount,self.classes];
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        self.courseTimes = [NSMutableArray array];
+    }
+    return self;
+}
+
 @end
--- iHBNU/Model/CourseTime.m
+++ iHBNU/Model/CourseTime.m
@@ -10,4 +10,8 @@
 
 @implementation CourseTime
 
+- (NSString *)description {
+    return [NSString stringWithFormat:@"%@\n\tweekday:%@\n\tsequence:%@\n\tstartWeek:%@\n\tendWeek:%@\n\toddWeak:%i\n\tevenWeak:%i",[super description], self.weekday,self.sequence,self.startWeek,self.endWeek,self.oddWeek,self.evenWeek];
+}
+
 @end
